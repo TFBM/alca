08b73a134191ca8623f18a7de214cceab10d896f
Lola Dam <contact@loladam.com>
2014-05-24 15:13:55 +0200

    Update add public key

--- home/forms.py
+++ home/forms.py
@@ -60,6 +60,22 @@ class EditPublicKForm(forms.Form):
         self.helper.add_input(Button('', "Cancel", css_class='btn btn-danger', css_id='publicK-cancel'))
 
     publicK = forms.CharField(label="", max_length=30, widget=forms.TextInput())
+    
+class AddPublicKForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super(AddPublicKForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'add'
+
+        self.helper.add_input(Submit('submit', 'Add'))
+        self.helper.add_input(Button('', "Cancel", css_class='btn btn-danger', css_id='publicK-cancel'))
+
+    value = forms.CharField(label="Public Key", max_length=130, widget=forms.TextInput())
+    name = forms.CharField(label="Name of the public key", max_length=30, widget=forms.TextInput())
+    comment = forms.CharField(label="Comment", widget=forms.Textarea())
+    order = forms.IntegerField(label="Order to be displayed", widget=forms.TextInput())
 
 class newTransactionForm(forms.Form):
 
--- home/views.py
+++ home/views.py
@@ -4,9 +4,17 @@ from home.forms import EditUsernameForm
 from home.forms import EditEmailForm
 from home.forms import EditBitmessageForm
 from home.forms import EditPublicKForm
+from home.forms import AddPublicKForm
 from home.forms import newTransactionForm
 
 from django.db import models
+from django.contrib.auth.models import User
+
+#Import database class pubKey
+from transactions.models import PubKey
+
+#To create token with email
+import hashlib
 
 #Added for email server
 from django.conf import settings
@@ -22,11 +30,21 @@ def home(request):
 def profil(request):
     if request.user.is_authenticated():
        logged = True
+	#Retrieve information of the user
        username = request.user.username
+       user_id = request.user.id
+
+       try :
+         pubKey = PubKey.objects.get(user = user_id)
+       except : 
+		   pubKey = 'None'
+
+	# Edit form for modification 
        username_form = EditUsernameForm()
        email_form = EditEmailForm()
        bitmessage_form = EditBitmessageForm()
        publicK_form = EditPublicKForm()
+       add_publicK_form = AddPublicKForm()
 
     return render(request, 'home/profil.html', locals())
 
@@ -58,20 +76,32 @@ def disputes(request):
        username = request.user.username
 
     return render(request, 'home/disputes.html', locals())
+    
+def add(request):
+    if request.method == "POST":
+        if "value" and "name" and "comment" and "order" in request.POST:
+            form = AddPublicKForm(request.POST)
+            if form.is_valid():
+               user_fk = User.objects.get(id=request.user.id)
+               p = PubKey(value = form.cleaned_data['value'], name = form.cleaned_data['name'], comment = form.cleaned_data['comment'], order = form.cleaned_data['order'], user = user_fk)
+               p.save()
+    return redirect("profil")
+
 
 def new(request):
-    if request.method == "POST":
-        if "good" and "description" and "price" and "buyer_email" in request.POST:
-            form = newTransactionForm(request.POST)
-            if form.is_valid():
-		message = "Someone want to do a transaction with you. Good : " + form.cleaned_data['good'] + "; Description : " + form.cleaned_data['description'] + "; Price : " + str(form.cleaned_data['price']) + 'Send the ' + time.strftime("%c")
-		try : 
-			send_mail('CryptoUtc - New Transaction request', message , settings.EMAIL_HOST_USER , [form.cleaned_data['buyer_email']], fail_silently=False)
+   if request.method == "POST":
+      if "good" and "description" and "price" and "buyer_email" in request.POST:
+         form = newTransactionForm(request.POST)
+         if form.is_valid():
+		      message = "Someone want to do a transaction with you. Good : " + form.cleaned_data['good'] + "; Description : " + form.cleaned_data['description'] + "; Price : " + str(form.cleaned_data['price']) + ", Token : " + hashlib.md5(form.cleaned_data['buyer_email']).hexdigest()
+		      #transaction = Transaction(good = form.cleaned_data['good'], description = form.cleaned_data['description'], price =  , pubkey = )
+		      try : 
+			      send_mail('CryptoUtc - New Transaction request', message , settings.EMAIL_HOST_USER , [form.cleaned_data['buyer_email']], fail_silently=False)
 			
-			return redirect("profil")
-		except BadHeaderError:
-			return HttpResponse('Invalid header found.')
-	else :
-		return redirect("disputes")
-    return redirect("transactions")
+			      return redirect("profil")
+		      except BadHeaderError:
+			      return HttpResponse('Invalid header found.')
+      else :
+		   return redirect("disputes")
+   return redirect("transactions")
 
