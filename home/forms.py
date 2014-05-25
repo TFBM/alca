d55f8e943ab271a117375822a29d4bd1bdcc5690
alecalve <antoine@alc.io>
2014-05-25 16:52:16 +0200

    Cleanup + création transaction (mais que en partie)

--- home/forms.py
+++ home/forms.py
@@ -80,17 +80,17 @@ class AddPublicKForm(forms.Form):
 class newTransactionForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
-        super(newTransactionForm, self).__init__()
+        super(newTransactionForm, self).__init__(*args)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
         self.helper.form_action = 'new'
 
         self.helper.add_input(Submit('submit', 'Send'))
-        if "pubKey" in kwargs : 
+        if "pubKey" in kwargs: 
             self.fields["pubKey"].choices = [(pk.value, "%s (%s)" % (pk.name, pk.value)) for pk in kwargs['pubKey']]
         
     good = forms.CharField(label="Good Name", max_length=255, widget=forms.TextInput())
-    description = forms.CharField(label="Description of the good", widget=forms.Textarea())
-    price = forms.DecimalField(label="Price asking", max_digits=BTC_MAX_DIGIT,decimal_places=BTC_DECIMAL, widget=forms.TextInput())
+    description = forms.CharField(label="Description of the good", max_length=2500, widget=forms.Textarea())
+    price = forms.DecimalField(label="Price asking (BTC)", max_digits=BTC_MAX_DIGIT,decimal_places=BTC_DECIMAL, widget=forms.TextInput())
     buyer_email = forms.CharField(label="Email buyer", max_length=255, widget=forms.TextInput())
     pubKey = forms.ChoiceField(label="Public Key", widget=forms.Select())
--- home/views.py
+++ home/views.py
@@ -1,17 +1,13 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render, redirect
-from home.forms import EditUsernameForm
-from home.forms import EditEmailForm
-from home.forms import EditBitmessageForm
-from home.forms import EditPublicKForm
-from home.forms import AddPublicKForm
-from home.forms import newTransactionForm
-
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods, require_POST
+from home.forms import EditUsernameForm, EditEmailForm, EditBitmessageForm, EditPublicKForm, AddPublicKForm, newTransactionForm
 from django.db import models
 from django.contrib.auth.models import User
 
 #Import database class pubKey
-from transactions.models import PubKey
+from transactions.models import PubKey, Transaction
 
 #Import time to get the time at creation of the transaction
 from datetime import datetime
@@ -24,96 +20,86 @@ from django.conf import settings
 from django.core.mail import send_mail
     
 def home(request):
-    if request.user.is_authenticated():
-       logged = True
-       username = request.user.username
-        
     return render(request, 'home/index.html', locals())
 
+@login_required
 def profil(request):
-    if request.user.is_authenticated():
-       logged = True
-	#Retrieve information of the user
-       username = request.user.username
-       user_id = request.user.id
+  #Retrieve information of the user
+  try:
+    pubKey = PubKey.objects.filter(user=request.user.id).order_by('order')
+  except:
+    pubKey = None
 
-       try :
-         pubKey = PubKey.objects.filter(user = user_id).order_by('order')
-       except : 
-		   pubKey = 'None'
 
-	# Edit form for modification 
-       username_form = EditUsernameForm()
-       email_form = EditEmailForm()
-       bitmessage_form = EditBitmessageForm()
-       publicK_form = EditPublicKForm()
-       add_publicK_form = AddPublicKForm()
+  # Edit form for modification 
+  username_form = EditUsernameForm()
+  email_form = EditEmailForm()
+  bitmessage_form = EditBitmessageForm()
+  publicK_form = EditPublicKForm()
+  add_publicK_form = AddPublicKForm()
 
-    return render(request, 'home/profil.html', locals())
+  return render(request, 'home/profil.html', locals())
 
+
+@login_required
 def transactions(request):
-    if request.user.is_authenticated():
-      logged = True
-      username = request.user.username
+  try :
+    pubKey = PubKey.objects.filter(user = request.user.id).order_by('order')
+  except:
+    #Todo: renvoyer sur la page permettant de créer une clé avec un message disant de le faire
+    pubKey = None
 
-      try :
-        pubKey = PubKey.objects.filter(user = request.user.id).order_by('order')
-      except:
-        pubKey = None
+  new_form = newTransactionForm(pubKey=pubKey)
 
-      print pubKey
-       
-      new_form = newTransactionForm(pubKey=pubKey)
+  return render(request, 'home/transactions.html', locals())
 
-    return render(request, 'home/transactions.html', locals())
-
+@login_required
+@require_POST
 def edit(request):
-    if request.method == "POST":
-        if "username" in request.POST:
-            form = EditUsernameForm(request.POST)
-            if form.is_valid():
-                request.user.username = form.cleaned_data['username']
-                request.user.save()
-	if "email" in request.POST:
-	    form = EditEmailForm(request.POST)
-	    if form.is_valid():
-		request.user.email = form.cleaned_data['email']
-                request.user.save()
-    return redirect("profil")
+  form = EditUsernameForm(request.POST)
+  if form.is_valid():
+    request.user.username = form.cleaned_data['username']
+    request.user.save()
+      
+  form = EditEmailForm(request.POST)
+  if form.is_valid():
+    request.user.email = form.cleaned_data['email']
+    request.user.save()
+  return redirect("profil")
 
+@login_required
 def disputes(request):
-    if request.user.is_authenticated():
-       logged = True
-       username = request.user.username
-
     return render(request, 'home/disputes.html', locals())
-    
+
+@login_required
+@require_POST
 def add(request):
-    if request.method == "POST":
-        if "value" and "name" and "comment" and "order" in request.POST:
-            form = AddPublicKForm(request.POST)
-            if form.is_valid():
-               user_fk = User.objects.get(id=request.user.id)
-               p = PubKey(value = form.cleaned_data['value'], name = form.cleaned_data['name'], comment = form.cleaned_data['comment'], order = form.cleaned_data['order'], user = user_fk)
-               p.save()
-    return redirect("profil")
+  form = AddPublicKForm(request.POST)
+  if form.is_valid():
+    user_fk = User.objects.get(id=request.user.id)
+    p = PubKey(value = form.cleaned_data['value'], name = form.cleaned_data['name'], comment = form.cleaned_data['comment'], order = form.cleaned_data['order'], user = user_fk)
+    p.save()
+  return redirect("profil")
 
-
+@login_required
+@require_POST
 def new(request):
-   if request.method == "POST":
-      if "good" and "description" and "price" and "buyer_email" in request.POST:
-         form = newTransactionForm(request.POST)
-         if form.is_valid():
-		      message = "Someone want to do a transaction with you. Good : " + form.cleaned_data['good'] + "; Description : " + form.cleaned_data['description'] + "; Price : " + str(form.cleaned_data['price']) + ", Token : " + hashlib.md5(form.cleaned_data['buyer_email']).hexdigest() + ", Seller public Key : " + str(form.cleaned_data['pubKey'])
-		      #seller_pubKey = PubKey.objects.get(value = form.cleaned_data['pubKey'])
-		      #transaction = Transaction(good = form.cleaned_data['good'], description = form.cleaned_data['description'], price = form.cleaned_data['price'] , seller = seller_pubKey , datetime_init = datetime.now())
-		      try : 
-			      send_mail('CryptoUtc - New Transaction request', message , settings.EMAIL_HOST_USER , [form.cleaned_data['buyer_email']], fail_silently=False)
-			      transaction.save()
-			      return redirect("profil")
-		      except BadHeaderError:
-			      return HttpResponse('Invalid header found.')
-      else :
-		   return redirect("disputes")
-   return redirect("home")
+  try:
+    pubKey = PubKey.objects.filter(user=request.user.id).order_by('order')
+  except:
+    pubKey = None
+    
+  form = newTransactionForm(request.POST, pubKey=pubKey)
 
+  if form.is_valid():
+    seller_pubKey = PubKey.objects.get(value = form.cleaned_data['pubKey'])
+    transaction = Transaction(good = form.cleaned_data['good'],
+			      description = form.cleaned_data['description'],
+			      price = form.cleaned_data['price'] ,
+			      seller = seller_pubKey ,
+			      datetime_init = datetime.now())
+    transaction.save()
+
+    return redirect("profil")
+  else:
+    return redirect("disputes")
