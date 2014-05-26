8b0957cc2c4b81c5641503b80f8383b1f6744e3f
Lola Dam <contact@loladam.com>
2014-05-27 00:37:48 +0200

    Ajout d'un fichier yaml escrow et modification creation de transaction

--- transactions/views.py
+++ transactions/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.contrib.auth.models import User
-from transactions.models import PubKey, Transaction
+from transactions.models import PubKey, PubkeyEscrow, Transaction
 from transactions.forms import newTransactionForm
 from datetime import datetime
 import hashlib
@@ -35,11 +35,15 @@ def new(request):
 
   if form.is_valid():
     seller_pubKey = PubKey.objects.get(value = form.cleaned_data['pubKey'])
+    #TODO Il faudrait un petit algo qui va choisir des clé publique au hasard non utilisé. Pour l'instant on prend tjrs la meme.
+    escrow_pubKey = PubkeyEscrow.objects.get(value='03355bc5d353e23ac5df9d1b2931f2d1c2fa931d2b5ee88154f3e963e752372c43')
     transaction = Transaction(good = form.cleaned_data['good'],
 			      description = form.cleaned_data['description'],
 			      price = form.cleaned_data['price'] ,
 			      seller = seller_pubKey ,
-			      datetime_init = datetime.now())
+			      escrow = escrow_pubKey ,
+			      datetime_init = datetime.now(),
+			      status = 1)
     transaction.save()
 
     return redirect("profil")
