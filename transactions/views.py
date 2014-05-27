3e1b224ae3afa72386eec8d59544ed53e7e85d4b
Lola Dam <contact@loladam.com>
2014-05-27 18:43:12 +0200

    Update affichage transactions dans transactions status

--- transactions/views.py
+++ transactions/views.py
@@ -19,6 +19,8 @@ def transactions(request):
     pubKey = None
 
   new_form = newTransactionForm(pubKey=pubKey)
+  
+  listTransactions = Transaction.objetcs.get()
 
   return render(request, 'home/transactions.html', locals())
 
