3169ffa0a3976f15b594522c00fc51a8c8460f6e
Lola Dam <contact@loladam.com>
2014-06-16 15:32:38 +0200

    ADDED page qui permet l'api d'update le transaction status de 2 à 3

--- transactions/urls.py
+++ transactions/urls.py
@@ -7,5 +7,8 @@ urlpatterns = patterns('transactions.views',
     url(r'^accept/(?P<id_transaction>\d+)/$', 'accept', name='accept'),
     url(r'^accept/(?P<id_transaction>\d+)/accept$', 'accept', name='accept'),
     url(r'^cancel/(?P<id_transaction>\d+)/$', 'cancel', name='cancel'),
+    url(r'^dispute/(?P<id_transaction>\d+)/$', 'dispute', name='dispute'),
+    url(r'^dispute/(?P<id_transaction>\d+)/new$', 'new', name='new'),
+    url(r'^update_status/(?P<id_transaction>\d+)/$', 'update_status', name='update_status'),
 )
 
